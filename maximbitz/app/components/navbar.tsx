8c6cdf08a4e552e49cc90988387d69513effe576
Kuraj11101 <smov.org@gmail.com>
2024-03-15 20:45:09 +0000

    pages

--- maximbitz/app/components/navbar.tsx
+++ maximbitz/app/components/navbar.tsx
@@ -297,24 +297,6 @@ export default function NavbarSimple() {
         >
           Maximbitz
         </Typography>
-        {/* <div className="relative flex w-full gap-2 md:w-max">
-          <Input
-            type="search"
-            color="white"
-            label="Type here..."
-            className="pr-20"
-            containerProps={{
-              className: "min-w-[288px]",
-            }}
-          />
-          <Button
-            size="sm"
-            color="white"
-            className="!absolute right-1 top-1 rounded"
-          >
-            Search
-          </Button>
-        </div> */}
         <div className="hidden lg:block">
           <NavList />
         </div>
